4a2859de27e6e9e67b2b862917b4d2d091a822ef
mehdi-sz <mehdi.zayani15@gmail.com>
2023-11-29 15:24:36 +0100

    ajout du conteneur header

---
+++ mz-frontend/src/constants/images.js
@@ -0,0 +1,16 @@
+import profile from '../assets/images/msz1.png';
+import circle from '../assets/images/circle.svg';
+import react from '../assets/images/react.png';
+import api from '../assets/images/api.png';
+import git from '../assets/images/git.png';
+import python from '../assets/images/python.png';
+import node from '../assets/images/node.png'
+export default {
+    profile,
+    circle,
+    react,
+    api,
+    git,
+    python,
+    node,
+};
--- mz-frontend/src/containers/Header/Header.jsx
+++ mz-frontend/src/containers/Header/Header.jsx
@@ -1,9 +1,71 @@
 import React from 'react'
 import './Header.scss'
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+
+import { motion } from 'framer-motion';
+import { images } from '../../constants';
+
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut',
+    },
+  },
+};
+
+const Header = () => (
+  <div className='app__header app__flex'>
+    <motion.div
+      whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+      transition={{ duration: 0.5 }}
+      className="app__header-info"
+    >
+    <div className="app__header-badge">
+        <div className="badge-cmp app__flex">
+          <span>👋</span>
+          <div style={{ marginLeft: 20 }}>
+            <p className="p-text">Hello, moi c'est</p>
+            <h1 className="head-text">Mehdi</h1>
+          </div>
+        </div>
+
+        <div className="tag-cmp app__flex">
+          <p className="p-text">Ingénieur d'études</p>
+          <p className="p-text">& de développement</p>
+        </div>
+      </div>
+      </motion.div>
+
+      <motion.div
+      whileInView={{ opacity: [0, 1] }}
+      transition={{ duration: 0.5, delayChildren: 0.5 }}
+      className="app__header-img"
+    >
+      <img src={images.profile} alt="profile_bg" />
+      <motion.img
+        whileInView={{ scale: [0, 1] }}
+        transition={{ duration: 1, ease: 'easeInOut' }}
+        src={images.circle}
+        alt="profile_circle"
+        className="overlay_circle"
+      />
+    </motion.div>
+    
+    <motion.div
+      variants={scaleVariants}
+      whileInView={scaleVariants.whileInView}
+      className="app__header-circles"
+    >
+      {[images.react, images.git, images.python, images.node].map((circle, index) => (
+        <div className="circle-cmp app__flex" key={`circle-${index}`}>
+          <img src={circle} alt="profile_bg" />
+        </div>
+      ))}
+    </motion.div>
+
+    </div>
+)
 
 export default Header
